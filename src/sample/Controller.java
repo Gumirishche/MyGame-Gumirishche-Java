754f49234daf23d283137f924b930a0d77cb3d03
Gumirishche <gumer2001@mail.ru>
2021-05-17 19:09:35 +0400

    2 комит(модифицировал переходы лунохода)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -43,198 +43,6 @@ public class Controller {
     @FXML
     private GridPane gridPane;
 
-    @FXML
-    private Pane c3;
-
-    @FXML
-    private Pane a2;
-
-    @FXML
-    private Pane a3;
-
-    @FXML
-    private Pane a4;
-
-    @FXML
-    private Pane a5;
-
-    @FXML
-    private Pane a6;
-
-    @FXML
-    private Pane a7;
-
-    @FXML
-    private Pane a8;
-
-    @FXML
-    private Pane b1;
-
-    @FXML
-    private Pane c1;
-
-    @FXML
-    private Pane d1;
-
-    @FXML
-    private Pane e1;
-
-    @FXML
-    private Pane f1;
-
-    @FXML
-    private Pane g1;
-
-    @FXML
-    private Pane r1;
-
-    @FXML
-    private Pane b2;
-
-    @FXML
-    private Pane b3;
-
-    @FXML
-    private Pane b4;
-
-    @FXML
-    private Pane b5;
-
-    @FXML
-    private Pane b6;
-
-    @FXML
-    private Pane b7;
-
-    @FXML
-    private Pane b8;
-
-    @FXML
-    private Pane c2;
-
-    @FXML
-    private Pane a1;
-
-    @FXML
-    private Pane c4;
-
-    @FXML
-    private Pane c5;
-
-    @FXML
-    private Pane c6;
-
-    @FXML
-    private Pane c7;
-
-    @FXML
-    private Pane c8;
-
-    @FXML
-    private Pane d2;
-
-    @FXML
-    private Pane d3;
-
-    @FXML
-    private Pane d4;
-
-    @FXML
-    private Pane d5;
-
-    @FXML
-    private Pane d6;
-
-    @FXML
-    private Pane d7;
-
-    @FXML
-    private Pane d8;
-
-    @FXML
-    private Pane e8;
-
-    @FXML
-    private Pane f8;
-
-    @FXML
-    private Pane g8;
-
-    @FXML
-    private Pane r8;
-
-    @FXML
-    private Pane e7;
-
-    @FXML
-    private Pane e6;
-
-    @FXML
-    private Pane e5;
-
-    @FXML
-    private Pane e4;
-
-    @FXML
-    private Pane e3;
-
-    @FXML
-    private Pane e2;
-
-    @FXML
-    private Pane f2;
-
-    @FXML
-    private Pane g2;
-
-    @FXML
-    private Pane r2;
-
-    @FXML
-    private Pane f7;
-
-    @FXML
-    private Pane f6;
-
-    @FXML
-    private Pane f5;
-
-    @FXML
-    private Pane f4;
-
-    @FXML
-    private Pane f3;
-
-    @FXML
-    private Pane g3;
-
-    @FXML
-    private Pane r3;
-
-    @FXML
-    private Pane g4;
-
-    @FXML
-    private Pane g5;
-
-    @FXML
-    private Pane g6;
-
-    @FXML
-    private Pane g7;
-
-    @FXML
-    private Pane r4;
-
-    @FXML
-    private Pane r5;
-
-    @FXML
-    private Pane r6;
-
-    @FXML
-    private Pane r7;
-
 
     @FXML
     void initialize() {
@@ -273,28 +81,32 @@ public class Controller {
             else if(d==2){
                 this.direction=direction;
                 if(ControllerDirection.direction.equals("up")){
+                    gridPane.add(new ImageView(new Image("File:pic/Moon.png")),moonRider1.y,moonRider1.x);
                     moonRider1.x=moonRider1.x-1;
                     moonRider1=Cell.findBy(moonRider1.x,moonRider1.y);
                     System.out.println(moonRider1);
-                    gridPane.add(new ImageView(new Image("File:pic/moonRider.png")),moonRider1.y,moonRider1.x);
+                    gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")),moonRider1.y,moonRider1.x);
                 }
                 if(ControllerDirection.direction.equals("down")){
+                    gridPane.add(new ImageView(new Image("File:pic/Moon.png")),moonRider1.y,moonRider1.x);
                     moonRider1.x=moonRider1.x+1;
                     moonRider1=Cell.findBy(moonRider1.x,moonRider1.y);
                     System.out.println(moonRider1);
-                    gridPane.add(new ImageView(new Image("File:pic/moonRider.png")),moonRider1.y,moonRider1.x);
+                    gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")),moonRider1.y,moonRider1.x);
                 }
                 if(ControllerDirection.direction.equals("left")){
+                    gridPane.add(new ImageView(new Image("File:pic/Moon.png")),moonRider1.y,moonRider1.x);
                     moonRider1.y=moonRider1.y-1;
                     moonRider1=Cell.findBy(moonRider1.x,moonRider1.y);
                     System.out.println(moonRider1);
-                    gridPane.add(new ImageView(new Image("File:pic/moonRider.png")),moonRider1.y,moonRider1.x);
+                    gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")),moonRider1.y,moonRider1.x);
                 }
                 if(ControllerDirection.direction.equals("right")){
+                    gridPane.add(new ImageView(new Image("File:pic/Moon.png")),moonRider1.y,moonRider1.x);
                     moonRider1.y=moonRider1.y+1;
                     moonRider1=Cell.findBy(moonRider1.x,moonRider1.y);
                     System.out.println(moonRider1);
-                    gridPane.add(new ImageView(new Image("File:pic/moonRider.png")),moonRider1.y,moonRider1.x);
+                    gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")),moonRider1.y,moonRider1.x);
                 }
             }
         });
