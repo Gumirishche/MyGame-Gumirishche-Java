6ce283e53ba6f65a45f3270068c03697b3ac968b
Gumirishche <gumer2001@mail.ru>
2021-05-17 22:06:46 +0400

    4 комит(сделал стены и выстрелы)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,6 +14,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -21,10 +22,10 @@ import javafx.scene.layout.GridPane;
 
 public class Controller {
     public int d = 0;
-    public String direction="";
+    public String direction = "";
     public static Stage stage = new Stage();
-    int[][] map=new int[8][8];
-    public Cell moonRider1=Cell.R1;
+    int[][] walls = new int[14][2];
+    public Cell moonRider1 = Cell.R1;
     @FXML
     private ResourceBundle resources;
 
@@ -77,45 +78,102 @@ public class Controller {
             stage.showAndWait();
         });
         goButton.setOnAction(actionEvent -> {
-            if(d==1){}
-            else if(d==2){
-                this.direction=direction;
-                if(ControllerDirection.direction.equals("up")){
-                    gridPane.add(new ImageView(new Image("File:pic/Moon.png")),moonRider1.y,moonRider1.x);
-                    moonRider1.x=moonRider1.x-1;
-                    moonRider1=Cell.findBy(moonRider1.x,moonRider1.y);
-                    System.out.println(moonRider1);
-                    gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")),moonRider1.y,moonRider1.x);
+            if (d == 1) {
+                this.direction = direction;
+                if (ControllerDirection.direction.equals("up")) {
+                    for (int i = 0; i < moonRider1.x; i++) {
+                        if (new Walls().canShoot((moonRider1.x - 1) - i, moonRider1.y)) {
+                            gridPane.add(new ImageView(new Image("File:pic/shot.png")), moonRider1.y, (moonRider1.x - 1) - i);
+                            break;
+                        } else {
+                            gridPane.add(new ImageView(new Image("File:pic/shot.png")), moonRider1.y, (moonRider1.x - 1) - i);
+                        }
+                    }
                 }
-                if(ControllerDirection.direction.equals("down")){
-                    gridPane.add(new ImageView(new Image("File:pic/Moon.png")),moonRider1.y,moonRider1.x);
-                    moonRider1.x=moonRider1.x+1;
-                    moonRider1=Cell.findBy(moonRider1.x,moonRider1.y);
-                    System.out.println(moonRider1);
-                    gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")),moonRider1.y,moonRider1.x);
+                if (ControllerDirection.direction.equals("down")) {
+                    for (int i = 0; i < 7 - moonRider1.x; i++) {
+                        if (new Walls().canShoot((moonRider1.x + 1) + i, moonRider1.y)) {
+                            gridPane.add(new ImageView(new Image("File:pic/shot.png")), moonRider1.y, (moonRider1.x + 1) + i);
+                            break;
+                        } else {
+                            gridPane.add(new ImageView(new Image("File:pic/shot.png")), moonRider1.y, (moonRider1.x + 1) + i);
+                        }
+                    }
                 }
-                if(ControllerDirection.direction.equals("left")){
-                    gridPane.add(new ImageView(new Image("File:pic/Moon.png")),moonRider1.y,moonRider1.x);
-                    moonRider1.y=moonRider1.y-1;
-                    moonRider1=Cell.findBy(moonRider1.x,moonRider1.y);
-                    System.out.println(moonRider1);
-                    gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")),moonRider1.y,moonRider1.x);
+                if (ControllerDirection.direction.equals("left")) {
+                    for (int i = 0; i < moonRider1.y; i++) {
+                        if (new Walls().canShoot(moonRider1.x, (moonRider1.y - 1) - i)) {
+                            gridPane.add(new ImageView(new Image("File:pic/shot.png")), (moonRider1.y - 1) - i, moonRider1.x);
+                            break;
+                        } else {
+                            gridPane.add(new ImageView(new Image("File:pic/shot.png")), (moonRider1.y - 1) - i, moonRider1.x);
+                        }
+                    }
                 }
-                if(ControllerDirection.direction.equals("right")){
-                    gridPane.add(new ImageView(new Image("File:pic/Moon.png")),moonRider1.y,moonRider1.x);
-                    moonRider1.y=moonRider1.y+1;
-                    moonRider1=Cell.findBy(moonRider1.x,moonRider1.y);
-                    System.out.println(moonRider1);
-                    gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")),moonRider1.y,moonRider1.x);
+                if (ControllerDirection.direction.equals("right")) {
+                    for (int i = 0; i < 7 - moonRider1.y; i++) {
+                        if (new Walls().canShoot(moonRider1.x, (moonRider1.y + 1) + i)) {
+                            gridPane.add(new ImageView(new Image("File:pic/shot.png")), (moonRider1.y + 1) + i, moonRider1.x);
+                            break;
+                        } else {
+                            gridPane.add(new ImageView(new Image("File:pic/shot.png")), (moonRider1.y + 1) + i, moonRider1.x);
+                        }
+                    }
+                }
+            } else if (d == 2) {
+                this.direction = direction;
+                if (ControllerDirection.direction.equals("up")) {
+                    if (new Walls().canGo(moonRider1.x - 1, moonRider1.y)) {
+                    } else {
+                        gridPane.add(new ImageView(new Image("File:pic/Moon.png")), moonRider1.y, moonRider1.x);
+                        moonRider1.x = moonRider1.x - 1;
+                        moonRider1 = Cell.findBy(moonRider1.x, moonRider1.y);
+                        System.out.println(moonRider1);
+                        gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")), moonRider1.y, moonRider1.x);
+                    }
+                }
+                if (ControllerDirection.direction.equals("down")) {
+                    if (new Walls().canGo(moonRider1.x + 1, moonRider1.y)) {
+                    } else {
+                        gridPane.add(new ImageView(new Image("File:pic/Moon.png")), moonRider1.y, moonRider1.x);
+                        moonRider1.x = moonRider1.x + 1;
+                        moonRider1 = Cell.findBy(moonRider1.x, moonRider1.y);
+                        System.out.println(moonRider1);
+                        gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")), moonRider1.y, moonRider1.x);
+                    }
+                }
+                if (ControllerDirection.direction.equals("left")) {
+                    if (new Walls().canGo(moonRider1.x, moonRider1.y - 1)) {
+                    } else {
+                        gridPane.add(new ImageView(new Image("File:pic/Moon.png")), moonRider1.y, moonRider1.x);
+                        moonRider1.y = moonRider1.y - 1;
+                        moonRider1 = Cell.findBy(moonRider1.x, moonRider1.y);
+                        System.out.println(moonRider1);
+                        gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")), moonRider1.y, moonRider1.x);
+                    }
+                }
+                if (ControllerDirection.direction.equals("right")) {
+                    if (new Walls().canGo(moonRider1.x, moonRider1.y + 1)) {
+                    } else {
+                        gridPane.add(new ImageView(new Image("File:pic/Moon.png")), moonRider1.y, moonRider1.x);
+                        moonRider1.y = moonRider1.y + 1;
+                        moonRider1 = Cell.findBy(moonRider1.x, moonRider1.y);
+                        System.out.println(moonRider1);
+                        gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")), moonRider1.y, moonRider1.x);
+                    }
                 }
             }
         });
-        for(int i=0;i!=8;i++){
-            for (int j=0;j!=8;j++){
-                gridPane.add(new ImageView(new Image("File:pic/Moon.png")),i,j);
+        for (int i = 0; i != 8; i++) {
+            for (int j = 0; j != 8; j++) {
+                gridPane.add(new ImageView(new Image("File:pic/Moon.png")), i, j);
             }
         }
-        gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")),7,0);
-        gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")),0,7);
+        walls = new Walls().wallsPosition();
+        for (int i = 0; i < 14; i++) {
+            gridPane.add(new ImageView(new Image("File:pic/Walls.png")), walls[i][0], walls[i][1]);
+        }
+        gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")), 7, 0);
+        gridPane.add(new ImageView(new Image("File:pic/MoonRider.png")), 0, 7);
     }
 }
---
+++ src/sample/Walls.java
@@ -0,0 +1,45 @@
+package sample;
+
+public class Walls {
+    public static int[][] walls = new int[14][2];
+    public static boolean canGo=true;
+
+    public int[][] wallsPosition() {
+        for (int i = 0; i < 14; i++) {
+            for (int j = 0; j < 2; j++) {
+                walls[i][j] = (int) (Math.random() * 8);
+            }
+        }
+        return walls;
+    }
+
+    public boolean canGo(int y,int x) {
+        for (int i=0;i<14;i++){
+            if (x== walls[i][0]&&y==walls[i][1]){
+                System.out.println(i+"nine");
+                canGo=true;
+                break;
+            }
+            else {
+                canGo=false;
+            }
+        }
+        return canGo;
+    }
+
+    public boolean canShoot(int y,int x) {
+        for (int i=0;i<14;i++){
+            if (x== walls[i][0]&&y==walls[i][1]){
+                System.out.println(i+"nine");
+                walls[i][0]=8;
+                walls[i][1]=8;
+                canGo=true;
+                break;
+            }
+            else {
+                canGo=false;
+            }
+        }
+        return canGo;
+    }
+}
