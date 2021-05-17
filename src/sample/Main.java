68c432c34d4c91813b3c83adeaf8888f30ff98d3
Golovushkin <golovushkin_aa@mail.ru>
2021-05-17 22:32:46 +0400

    типа сервер

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,8 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main extends Application {
-    public static Stage stage=new Stage();
+    public static Stage stage = new Stage();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -17,8 +21,24 @@ public class Main extends Application {
         stage.show();
     }
 
-
-    public static void main(String[] args) {
+    // private static ServerSocket server;
+    public static void main(String[] args) throws IOException, InterruptedException {
         launch(args);
+        // запустим пул нитей в которых колличество возможных нитей ограничено -
+        // 10-ю.
+        ExecutorService exec = Executors.newFixedThreadPool(10);
+        int j = 0;
+
+        // стартуем цикл в котором с паузой в 10 милисекунд стартуем Runnable
+        // клиентов,
+        // которые пишут какое-то количество сообщений
+        while (j < 10) {
+            j++;
+            exec.execute(new TestRunnableClientTester());
+            Thread.sleep(10);
+        }
+
+        // закрываем фабрику
+        exec.shutdown();
     }
 }
